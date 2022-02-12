923f94d1600cb34a9852158d7da8394a15edbbcd
jahangiralam7864 <wedevs95@gmail.com>
2022-02-12 12:25:48 +0600

    style

--- bank.js
+++ bank.js
@@ -5,11 +5,7 @@ const userEmail = emailField.value;
 //get user password input
 const passField = document.getElementById('user-pass');
 const userPass = passField.value;
-
 if (userEmail == 'jahangir@gmail.com' && userPass == 'jahangir') {  
   window.location.href = 'banking.html';
 }
-})
-
-// deposit handiling add
- 
+});
